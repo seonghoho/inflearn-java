f4b8bfe161f8365a4b4dd1ec0c8d595e1834fad6
choiseongho <chltjdgh3@naver.com>
2023-12-03 14:50:55 +0900

    [231203] 자바 조건문 학습

---
+++ java-start/src/cond/Switch2.java
@@ -0,0 +1,24 @@
+package cond;
+
+public class Switch2 {
+
+    public static void main(String[] args) {
+        int grade = 2;
+
+        int coupon;
+        switch (grade) {
+            case 1:
+                coupon = 1000;
+                break; // break가 없다면 계속 실행된다.
+            case 2:
+                coupon = 2000;
+                break;
+            case 3:
+                coupon = 3000;
+                break;
+            default:
+                coupon = 500;
+        }
+        System.out.println("발급받은 쿠폰 " + coupon);
+    }
+}
